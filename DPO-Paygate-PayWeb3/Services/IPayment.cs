7ca4cfaf4da2852dcf856e1663473a8840ca3499
wiredmartian <11290243+wiredmartian@users.noreply.github.com>
2019-05-08 01:10:41 +0200

    transaction

---
+++ DPO-Paygate-PayWeb3/Services/IPayment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPO_Paygate_PayWeb3.Services
+{
+    public interface IPayment
+    {
+        string ToUrlEncodedString(Dictionary<string, string> request);
+        Dictionary<string, string> ToDictionary(string response);
+        void AddTransaction(Dictionary<string, string> request, string payRequestId);
+        void AddTransactionReponseMeta(Dictionary<string, string> request, string PayrequestId);
+        Transaction GetTransaction(string payRequestId);
+        string GetMd5Hash(Dictionary<string, string> data, string encryptionKey);
+        bool VerifyMd5Hash(Dictionary<string, string> data, string encryptionKey, string hash);
+        MailAddress GetAuthenticatedUser();
+        void UpdateTransactionStatus(Transaction transaction);
+    }
+}
---
+++ DPO-Paygate-PayWeb3/Services/Payment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DPO_Paygate_PayWeb3.Services
+{
+    public class Payment
+    {
+    }
+}
