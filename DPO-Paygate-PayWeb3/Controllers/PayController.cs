67e336cb736d6bef00889c3105f44a90b7d102e0
wiredmartian <11290243+wiredmartian@users.noreply.github.com>
2019-05-08 02:28:23 +0200

    config

--- DPO-Paygate-PayWeb3/Controllers/PayController.cs
+++ DPO-Paygate-PayWeb3/Controllers/PayController.cs
@@ -1,4 +1,5 @@
-﻿using DPO_Paygate_PayWeb3.Services;
+﻿using DPO_Paygate_PayWeb3.Models;
+using DPO_Paygate_PayWeb3.Services;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -34,7 +35,7 @@ namespace DPO_Paygate_PayWeb3.Controllers
             request.Add("REFERENCE", "#45846"); // Payment ref e.g ORDER NUMBER
             request.Add("AMOUNT", paymentAmount);
             request.Add("CURRENCY", "ZAR"); // South Africa
-            request.Add("RETURN_URL", $"{Request.Url.Scheme}://{Request.Url.Authority}/paygate/postpayment");
+            request.Add("RETURN_URL", $"{Request.Url.Scheme}://{Request.Url.Authority}/pay/completepayment");
             request.Add("TRANSACTION_DATE", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
             request.Add("LOCALE", "en-za");
             request.Add("COUNTRY", "ZAF");
@@ -93,6 +94,119 @@ namespace DPO_Paygate_PayWeb3.Controllers
                 success = false,
                 message = "Failed to record a transaction"
             }, JsonRequestBehavior.AllowGet);
+        }
+
+        // This is your return url from Paygate
+        // This will run when you complete payment
+        [HttpPost]
+        public async Task<ActionResult> CompletePayment()
+        {
+            string responseContent = Request.Params.ToString();
+            Dictionary<string, string> results = _payment.ToDictionary(responseContent);
+
+            Transaction transaction = _payment.GetTransaction(results["PAY_REQUEST_ID"]);
+
+            if (transaction == null)
+            {
+                // Unable to reconsile transaction
+                return RedirectToAction("Failed");
+            }
+
+            // Reorder attributes for MD5 check
+            Dictionary<string, string> validationSet = new Dictionary<string, string>();
+            validationSet.Add("PAYGATE_ID", PayGateID);
+            validationSet.Add("PAY_REQUEST_ID", results["PAY_REQUEST_ID"]);
+            validationSet.Add("TRANSACTION_STATUS", results["TRANSACTION_STATUS"]);
+            validationSet.Add("REFERENCE", transaction.REFERENCE);
+
+            if (!_payment.VerifyMd5Hash(validationSet, PayGateKey, results["CHECKSUM"]))
+            {
+                // checksum error
+                return RedirectToAction("Failed");
+            }
+            /** Payment Status 
+             * -2 = Unable to reconsile transaction
+             * -1 = Checksum Error
+             * 0 = Pending
+             * 1 = Approved
+             * 2 = Declined
+             * 3 = Cancelled
+             * 4 = User Cancelled
+             */
+            int paymentStatus = int.Parse(results["TRANSACTION_STATUS"]);
+            if(paymentStatus == 1)
+            {
+                // Yey, payment approved
+                // Do something useful
+            }
+            // Query paygate transaction details
+            // And update user transaction on your database
+            await VerifyTransaction(responseContent, transaction.REFERENCE);
+            return RedirectToAction("Complete", new { id = results["TRANSACTION_STATUS"] });
+        }
+
+        private async Task VerifyTransaction(string responseContent, string Referrence)
+        {
+            HttpClient client = new HttpClient();
+            Dictionary<string, string> response = _payment.ToDictionary(responseContent);
+            Dictionary<string, string> request = new Dictionary<string, string>();
+
+            request.Add("PAYGATE_ID", PayGateID);
+            request.Add("PAY_REQUEST_ID", response["PAY_REQUEST_ID"]);
+            request.Add("REFERENCE", Referrence);
+            request.Add("CHECKSUM", _payment.GetMd5Hash(request, PayGateKey));
+
+            string requestString = _payment.ToUrlEncodedString(request);
+
+            StringContent content = new StringContent(requestString, Encoding.UTF8, "application/x-www-form-urlencoded");
+
+            // ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            HttpResponseMessage res = await client.PostAsync("https://secure.paygate.co.za/payweb3/query.trans", content);
+            res.EnsureSuccessStatusCode();
+
+            string _responseContent = await res.Content.ReadAsStringAsync();
+
+            Dictionary<string, string> results = _payment.ToDictionary(_responseContent);
+            if (!results.Keys.Contains("ERROR"))
+            {
+                _payment.UpdateTransaction(results, results["PAY_REQUEST_ID"]);
+            }
+
+        }
+
+        public ViewResult Complete(int? id)
+        {
+            string status = "Unknown";
+            switch (id.ToString())
+            {
+                case "-2":
+                    status = "Unable to reconsile transaction";
+                    break;
+                case "-1":
+                    status = "Checksum Error. The values have been altered";
+                    break;
+                case "0":
+                    status = "Not Done";
+                    break;
+                case "1":
+                    status = "Approved";
+                    break;
+                case "2":
+                    status = "Declined";
+                    break;
+                case "3":
+                    status = "Cancelled";
+                    break;
+                case "4":
+                    status = "User Cancelled";
+                    break;
+                default:
+                    status = $"Unknown Status({ id })";
+                    break;
+            }
+            TempData["Status"] = status;
+
+            return View();
         }
     }
 }
